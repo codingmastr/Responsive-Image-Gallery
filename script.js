47682d8e85a29d57479fdb7eb215362cbedbd3ea
Tarun Kaushik <codingmaster52@gmail.com>
2023-12-06 16:21:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// function openModal(imageSrc) {
+//     const modal = document.getElementById("modal");
+//     const modalImage = document.getElementById("modalImage");
+
+//     modal.style.display = "block";
+//     modalImage.src = imageSrc;
+// }
+
+// function closeModal() {
+//     const modal = document.getElementById("modal");
+//     modal.style.display = "none";
+// }
